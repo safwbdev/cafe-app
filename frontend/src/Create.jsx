62e6f3d10ded3b36e5793ff1b71a0d009d251c3b
Stefaan <safwbdev@gmail.com>
2024-08-19 23:04:11 +0800

    Update create form

--- frontend/src/Create.jsx
+++ frontend/src/Create.jsx
@@ -7,6 +7,10 @@ import AddIcon from '@mui/icons-material/Add';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import Box from '@mui/material/Box';
+import Checkbox from '@mui/material/Checkbox';
+import FormGroup from '@mui/material/FormGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+
 const style = {
     position: 'absolute',
     top: '50%',
@@ -19,13 +23,21 @@ const style = {
     p: 4,
 };
 const Create = () => {
-    const [entry, setEntry] = useState('');
+    const [name, setName] = useState('');
+    const [location, setLocation] = useState('');
+    const [map, setMap] = useState('');
+    const [menu, setMenu] = useState('');
+    const [type, setType] = useState(0);
+    const [vegetarian, setVegetarian] = useState(false);
+    const [halal, setHalal] = useState(false);
+
+
     const [open, setOpen] = React.useState(false);
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
     const handleAdd = () => {
-        axios.post(`${import.meta.env.VITE_APP_URL}/add`, { name: entry })
+        axios.post(`${import.meta.env.VITE_APP_URL}/add`, { name: name, map: map, menu: menu, type: type, vegetarian: vegetarian, halal: halal })
             .then(result => location.reload())
             .catch(err => console.log(err))
     }
@@ -44,7 +56,15 @@ const Create = () => {
                     <Typography id="modal-modal-title" variant="h6" component="h2">
                         New Entry
                     </Typography>
-                    <TextField id="" label="Enter name" variant="outlined" onChange={(e) => setEntry(e.target.value)} />
+                    <TextField id="" label="Name" variant="outlined" onChange={(e) => setName(e.target.value)} />
+                    <TextField id="" label="Location" variant="outlined" onChange={(e) => setLocation(e.target.value)} />
+                    <TextField id="" label="Map" variant="outlined" onChange={(e) => setMap(e.target.value)} />
+                    <TextField id="" label="Menu" variant="outlined" onChange={(e) => setMenu(e.target.value)} />
+
+                    <FormGroup>
+                        <FormControlLabel control={<Checkbox checked={vegetarian} />} label="Vegetarian Friendly" onClick={() => setVegetarian(!vegetarian)} />
+                        <FormControlLabel control={<Checkbox checked={halal} />} label="Halal" onClick={() => setHalal(!halal)} />
+                    </FormGroup>
                     <Button variant="contained" onClick={handleAdd}>Add</Button>
                 </Box>
             </Modal>
